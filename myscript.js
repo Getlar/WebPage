8adaf06af434d4b9c036fae43730382b7070d0ad
Ranuon <tamastheactual@gmail.com>
2020-04-05 19:35:12 +0200

    Forms - SSE

--- myscript.js
+++ myscript.js
@@ -1,3 +1,19 @@
-MyFunc(){
-  
+var w;
+
+function startWorker() {
+  if (typeof(Worker) !== "undefined") {
+    if (typeof(w) == "undefined") {
+      w = new Worker("myscript.js");
+    }
+    w.onmessage = function(event) {
+      document.getElementById("result").innerHTML = event.data;
+    };
+  } else {
+    document.getElementById("result").innerHTML = "Sorry! No Web Worker support.";
+  }
+}
+
+function stopWorker() {
+  w.terminate();
+  w = undefined;
 }
