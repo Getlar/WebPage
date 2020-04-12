c22d764f4589368271b781c46b67d5c2993a1930
Getlar <tamastheactual@gmail.com>
2020-04-12 19:20:48 +0200

    Form Start

--- myscript.js
+++ myscript.js
@@ -1,7 +1,6 @@
 
 var mybutton = document.getElementById("myBtn");
 
-
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -15,4 +14,29 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+function returnMain(){
+  window.location.href = "index.html";
+}
+
+function goToTest(){
+  window.location.href = "test.html";
+}
+
+<script>
+document.getElementById("form1").onsubmit=function() {
+       variable = parseInt(document.querySelector('input[name = "variable"]:checked').value);
+	   sub = parseInt(document.querySelector('input[name = "sub"]:checked').value);
+	   con = parseInt(document.querySelector('input[name = "con"]:checked').value);
+	   ifstate = parseInt(document.querySelector('input[name = "ifstate"]:checked').value);
+
+
+	   result = variable + sub + con + ifstate;
+
+	document.getElementById("grade").innerHTML = result;
+
+
+
+return false; // required to not refresh the page; just leave this here
 }
