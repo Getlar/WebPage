23dafd1b5ca4dd9a89aa2847edb57350f5b03bf0
Getlar <tamastheactual@gmail.com>
2020-04-12 22:39:14 +0200

    Gallery Full Form Full

--- myscript.js
+++ myscript.js
@@ -24,19 +24,27 @@ function goToTest(){
   window.location.href = "test.html";
 }
 
-<script>
-document.getElementById("form1").onsubmit=function() {
-       variable = parseInt(document.querySelector('input[name = "variable"]:checked').value);
-	   sub = parseInt(document.querySelector('input[name = "sub"]:checked').value);
-	   con = parseInt(document.querySelector('input[name = "con"]:checked').value);
-	   ifstate = parseInt(document.querySelector('input[name = "ifstate"]:checked').value);
+document.getElementById("aforma").onsubmit=function() {
+     fnatic1 = parseInt(document.querySelector('input[name = "vb"]:checked').value);
+	   dk1 = parseInt(document.querySelector('input[name = "kupa"]:checked').value);
+	   sktt1 = parseInt(document.querySelector('input[name = "kupa2"]:checked').value);
+	   alex1 = parseInt(document.querySelector('input[name = "bd"]:checked').value);
+     usa2 = parseInt(document.querySelector('input[name = "vb2"]:checked').value);
+	   brand1 = parseInt(document.querySelector('input[name = "elso"]:checked').value);
+	   kayle1 = parseInt(document.querySelector('input[name = "rew"]:checked').value);
+	   season41 = parseInt(document.querySelector('input[name = "rh"]:checked').value);
+     g2014 = parseInt(document.querySelector('input[name = "cigi"]:checked').value);
+	   season62 = parseInt(document.querySelector('input[name = "ui"]:checked').value);
+	   masteryi1 = parseInt(document.querySelector('input[name = "tamas"]:checked').value);
+	   norbi1 = parseInt(document.querySelector('input[name = "norbi"]:checked').value);
+     kristof2 = parseInt(document.querySelector('input[name = "kristof"]:checked').value);
+	   fiddle1 = parseInt(document.querySelector('input[name = "anna"]:checked').value);
+	   illaoi1 = parseInt(document.querySelector('input[name = "frunci"]:checked').value);
 
 
-	   result = variable + sub + con + ifstate;
+	   result = fnatic1 + dk1 + sktt1 + alex1 + usa2 + brand1 + katyle1 + season41 + g2014 + season62 + masteryi1 + norbi1 + kristof2 + fiddle1 + illaoi1;
 
 	document.getElementById("grade").innerHTML = result;
-
-
 
 return false; // required to not refresh the page; just leave this here
 }
