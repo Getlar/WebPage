6f1189a0fc80b42803327cee2360045556bd17b7
Getlar <tamastheactual@gmail.com>
2020-04-12 23:32:19 +0200

    Result Shown

--- myscript.js
+++ myscript.js
@@ -24,27 +24,28 @@ function goToTest(){
   window.location.href = "test.html";
 }
 
-document.getElementById("aforma").onsubmit=function() {
-     fnatic1 = parseInt(document.querySelector('input[name = "vb"]:checked').value);
-	   dk1 = parseInt(document.querySelector('input[name = "kupa"]:checked').value);
-	   sktt1 = parseInt(document.querySelector('input[name = "kupa2"]:checked').value);
-	   alex1 = parseInt(document.querySelector('input[name = "bd"]:checked').value);
-     usa2 = parseInt(document.querySelector('input[name = "vb2"]:checked').value);
-	   brand1 = parseInt(document.querySelector('input[name = "elso"]:checked').value);
-	   kayle1 = parseInt(document.querySelector('input[name = "rew"]:checked').value);
-	   season41 = parseInt(document.querySelector('input[name = "rh"]:checked').value);
-     g2014 = parseInt(document.querySelector('input[name = "cigi"]:checked').value);
-	   season62 = parseInt(document.querySelector('input[name = "ui"]:checked').value);
-	   masteryi1 = parseInt(document.querySelector('input[name = "tamas"]:checked').value);
-	   norbi1 = parseInt(document.querySelector('input[name = "norbi"]:checked').value);
-     kristof2 = parseInt(document.querySelector('input[name = "kristof"]:checked').value);
-	   fiddle1 = parseInt(document.querySelector('input[name = "anna"]:checked').value);
-	   illaoi1 = parseInt(document.querySelector('input[name = "frunci"]:checked').value);
-
-
-	   result = fnatic1 + dk1 + sktt1 + alex1 + usa2 + brand1 + katyle1 + season41 + g2014 + season62 + masteryi1 + norbi1 + kristof2 + fiddle1 + illaoi1;
-
-	document.getElementById("grade").innerHTML = result;
-
-return false; // required to not refresh the page; just leave this here
+function kiErtekelo() {
+  var fnatic1 = parseInt(document.querySelector('input[name = "vb"]:checked').value);
+	var dk1 = parseInt(document.querySelector('input[name = "kupa"]:checked').value);
+	var sktt1 = parseInt(document.querySelector('input[name = "kupa2"]:checked').value);
+	var alex1 = parseInt(document.querySelector('input[name = "bd"]:checked').value);
+  var usa2 = parseInt(document.querySelector('input[name = "vb2"]:checked').value);
+	var brand1 = parseInt(document.querySelector('input[name = "elso"]:checked').value);
+	var kayle1 = parseInt(document.querySelector('input[name = "rew"]:checked').value);
+	var season41 = parseInt(document.querySelector('input[name = "rh"]:checked').value);
+  var g2014 = parseInt(document.querySelector('input[name = "cigi"]:checked').value);
+	var season62 = parseInt(document.querySelector('input[name = "ui"]:checked').value);
+	var masteryi1 = parseInt(document.querySelector('input[name = "tamas"]:checked').value);
+	var norbi1 = parseInt(document.querySelector('input[name = "norbi"]:checked').value);
+  var kristof2 = parseInt(document.querySelector('input[name = "kristof"]:checked').value);
+	var fiddle1 = parseInt(document.querySelector('input[name = "anna"]:checked').value);
+	var illaoi1 = parseInt(document.querySelector('input[name = "frunci"]:checked').value);
+	var result = fnatic1 + dk1 + sktt1 + alex1 + usa2 + brand1 + kayle1 + season41 + g2014 + season62 + masteryi1 + norbi1 + kristof2 + fiddle1 + illaoi1;
+  var result2 = result/25;
+  alert("Eredményed: " + result2 + "/11");
+	document.getElementById("parag").element.value = result2;
+  return false; // required to not refresh the page; just leave this here
+}
+function reset(){
+  window.location.href = "test.html";
 }
